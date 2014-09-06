3d165857b94108354f097b1b0be8bd3b460e8649
Aaron Magil <amagil@camsys.com>
2014-09-06 16:55:38 -0400

    Devise added to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable,  :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :lockable, :validatable
 end
