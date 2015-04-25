e1201301b659b0f215b02e6e9ec9157a62e486a8
Aaron Magil <aaron@societyofgrownups.com>
2015-04-25 19:18:26 -0400

    Add location to event_params

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -88,7 +88,7 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.require(:event).permit(:name, :owner_email, :threshold, :name, :time, :description, :deadline, :invitee_emails )
+      params.require(:event).permit(:name, :owner_email, :threshold, :name, :time, :description, :deadline, :invitee_emails, :location )
     end
 
     def confirm_owner
