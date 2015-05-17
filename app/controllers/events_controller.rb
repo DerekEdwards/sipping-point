4de4f1d8f20600a3056d5dc82a267a18436bded1
Derek Edwards <dedwards8@gmail.com>
2015-05-17 11:34:40 -0400

    Fixes #48.  Go directly to edit page after creating an event.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -40,7 +40,7 @@ class EventsController < ApplicationController
 
     respond_to do |format|
       if @event.save
-        format.html { redirect_to event_path(@event), notice: 'Event was successfully created.' }
+        format.html { redirect_to edit_event_path(@event), notice: 'Event was successfully created.' }
         format.json { render :show, status: :created, location: @event }
       else
         format.html { render :new }
