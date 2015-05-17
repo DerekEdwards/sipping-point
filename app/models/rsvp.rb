0e10843a059ef31e83d8b8d6b60104cff0c647f7
Derek Edwards <dedwards8@gmail.com>
2015-05-16 21:56:10 -0400

    #11 Add 12 hour deleay before moving upcoming events to the past.  Allows for current events to appear on the My Events page.

--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -18,7 +18,7 @@ class Rsvp < ActiveRecord::Base
   scope :said_yes, -> { where(response: 1) }
   scope :said_no, -> { where(response: 0) }
   scope :unanswered, -> { where(response: nil) }
-  scope :upcoming, -> { joins(:event).where("time >= ?", Time.now) }
+  scope :upcoming, -> { joins(:event).where("time >= ?", Time.now + 12*3600) }
 
   #Methods 
   def to_param
