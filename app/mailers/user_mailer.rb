22bc4acac6984e91971c74b4b1ac12c0e3969e33
Derek Edwards <dedwards8@gmail.com>
2015-06-13 12:31:48 -0400

    #34, Event emails are now from the event owner.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,24 +1,23 @@
 class UserMailer < ActionMailer::Base
-  default from: ENV['gmail_username']
 
   def invite_email(rsvp)
   	@user =  rsvp.user
   	@rsvp = rsvp
     @event = rsvp.event
-  	mail(to: @user.email, subject: @event.owner.display_name + " Invites You")
+  	mail(to: @user.email, subject: @event.owner.display_name + " Invites You", from: @event.owner.display_name)
   end
 
   def confirmation_email(rsvp)
   	@user = rsvp.user
   	@rsvp = rsvp
     @event = rsvp.event
-  	mail(to: @user.email, subject: @event.name + " is Happening!")
+  	mail(to: @user.email, subject: @event.name + " is Happening!", from: @event.owner.display_name)
   end 
 
   def expiration_email(rsvp)
   	@user = rsvp.user
   	@rsvp = rsvp
     @event = rsvp.event
-  	mail(to: @user.email, subject: @event.name + " is canceled")
+  	mail(to: @user.email, subject: @event.name + " is canceled", from: @event.owner.display_name)
   end
  end 
