f92ce0f48a33edc5dcbc9162e451d5f6c1eed8df
Derek Edwards <dedwards8@gmail.com>
2015-07-19 22:37:33 -0400

    Update version.

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module Bacchus
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Eastern Time (US & Canada)'
-    config.version = 'v0.3.0'
+    config.version = 'v0.3.1'
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
