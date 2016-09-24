314ebda185748c62b26490a21da55f766694086e
Derek Edwards <dedwards8@gmail.com>
2016-09-24 19:51:12 -0400

    Fies #178 Make SMTP Settings Generic and Configurable from ENV.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,10 +19,10 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
-   :address              => "smtp.gmail.com",
+   :address              => ENV['smtp_address'],
    :port                 => 587,
-   :user_name            => ENV['gmail_username'],
-   :password             => ENV['gmail_password'],
+   :user_name            => ENV['smtp_username'],
+   :password             => ENV['smtp_password'],
    :authentication       => "plain",
    :enable_starttls_auto => true
   }
